b3e66215bf5295c56ad51f38708309486745bf7d
luizbrito7 <luiz.brito@omnik.com.br>
2026-02-14 01:39:39 -0300

    feat: integrate pino logger and update health check logging

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import logger from "./o11y/logger.mjs";
+
 const app = express();
 
 app.use(express.json());
@@ -6,9 +8,10 @@ app.use(express.json());
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  logger.info(`Server is running on port ${PORT}`);
 });
 
 app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'OK' });
+  logger.info('Health check called');
+  res.status(200).json({ status: 'Ok' });
 });
