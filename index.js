603056f706b56ea7d65d8ad79e670580988806a9
luizbrito7 <luiz.brito@omnik.com.br>
2026-02-14 00:41:58 -0300

    chore: update server configuration and add health check endpoint

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK' });
 });
